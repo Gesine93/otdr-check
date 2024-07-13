ec0fc77b3dc9cc5198db0e23920e9e536570df96
Gesine Fengler <110592817+Gesine93@users.noreply.github.com>
2024-07-13 12:07:53 +0200

    Update OTDR_Rohdaten_xlsx_mit_Daempfung_Pruefung (1).py

--- raw data/OTDR_Rohdaten_xlsx_mit_Daempfung_Pruefung (1).py
+++ raw data/OTDR_Rohdaten_xlsx_mit_Daempfung_Pruefung (1).py
@@ -1,5 +1,3 @@
-# Das Programm geht durch alle relevanten Ordner und liest die Rohrnummer und Kabellänge  den Excel-OTDR-Protokollen aus. Die Werte werden anschließend in einer Excel-Datei gespeichert.
-
 import pandas as pd
 import glob
 from statistics import mean
@@ -7,7 +5,7 @@ import csv
 import os
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
-import import argparse
+import argparse
 import re
 
 GW_1310 = 4.95
@@ -15,12 +13,16 @@ GW_1550 = 3.93
 GW_1625 = 4.2
 
 parser = argparse.ArgumentParser(
-prog='OTDR Raw Data Checker', description='The program reads raw OTDR data in XLSX format and puts out the cable length and attenuation for each address and wavelength. It also checks if the attenuations are below the threshold value. Command line arguments: python otdr_check_rd.py [--files optional_path_to_directory] [--splices number_of_splices (integer or cell reference)] [--extra additional_attenuation]',epilog='')
+    prog='OTDR Raw Data Checker', 
+    description='The program reads raw OTDR data in XLSX format and outputs the cable length and attenuation for each address and wavelength. It also checks if the attenuations are below the threshold value. Command line arguments: python otdr_check_rd.py [--files optional_path_to_directory] [--splices number_of_splices (integer or cell reference)] [--extra additional_attenuation]',
+    epilog=''
+)
 parser.add_argument('-f', '--files')
 parser.add_argument('-s', '--splices')
 parser.add_argument('-e', '--extra')
-return parser.parse_args()
+args = parser.parse_args()
 argv = vars(args)
+
 try:
     path = argv["files"] or os.getcwd()
     os.chdir(path)
@@ -29,14 +31,14 @@ try:
         OTDR_writer = csv.writer(OTDR_file, delimiter=",", lineterminator="\r")
         OTDR_writer.writerow(
             [
-                "Adresse",
-                "Kabelnummer",
-                "Mittlere Kabellaenge",
-                "Abweichung",
-                "Kabellaengen",
-                "Spanloss 1310 [dB]",
-                "Spanloss 1550",
-                "Spanloss 1625",
+                "Address",
+                "Cable Number",
+                "Average Cable Length",
+                "Deviation",
+                "Cable Lengths",
+                "Span Loss 1310 [dB]",
+                "Span Loss 1550",
+                "Span Loss 1625",
                 "HA-KVz [m]",
             ]
         )
@@ -44,57 +46,48 @@ try:
             print(file)
             wb = load_workbook(file, data_only=True)
             sh = wb.worksheets[0]
-            rohr = "None"
+            pipe = "None"
             try:
-                if sh.cell(8, 1).value == "Kabel-ID":
-                    rohr = sh.cell(9, 1).value
+                if sh.cell(8, 1).value == "Cable ID":
+                    pipe = sh.cell(9, 1).value
                 elif sh.cell(8, 11).value == "Cable ID":
-                    rohr = sh.cell(9, 11).value
-            except Exception as e:
-                print("Ein Fehler ist aufgetreten I: ", str(e))
-            address = "None"
-            try: 
-                address = sh.cell(9,8).value + sh.cell(9,12).value
+                    pipe = sh.cell(9, 11).value
             except Exception as e:
                 print(e)
-            laenge = []
+            address = "None"
+            try:
+                address = sh.cell(9, 8).value + sh.cell(9, 12).value
+            except Exception as e:
+                print(e)
+            length = []
             span_1310 = []
             span_1550 = []
             span_1625 = []
             for x in range(1, len(wb.sheetnames)):
                 sheet = wb.worksheets[x]
-                kabel = sheet.cell(25, 4).value
+                cable = sheet.cell(25, 4).value
                 span = sheet.cell(25, 10).value
                 nm = sheet.cell(19, 1).value
                 try:
-                    kabel_float = float(kabel)
-                    laenge.append(kabel_float)
+                    cable_float = float(cable)
+                    length.append(cable_float)
                     if "1310" in str(nm):
                         try:
-                            try:
-                                span_1310.append(float(span))
-                            except Exception as e:
-                                print("Ein Fehler ist aufgetreten A: ", str(e))
+                            span_1310.append(float(span))
                         except Exception as e:
-                            print("Ein Fehler ist aufgetreten B: ",str(e))
+                            print(e)
                     elif "1550" in str(nm):
                         try:
-                            try:
-                                span_1550.append(float(span))
-                            except Exception as e:
-                                print("Ein Fehler ist aufgetreten C ", str(e))
+                            span_1550.append(float(span))
                         except Exception as e:
-                            print("Ein Fehler ist aufgetreten D ", str(e))
+                            print(e)
                     elif "1625" in str(nm):
                         try:
-                            try:
-                                span_1625.append(float(span))
-                            except Exception as e:
-                                print("Ein Fehler ist aufgetreten E" , str(e))
+                            span_1625.append(float(span))
                         except Exception as e:
-                            print("Ein Fehler ist aufgetreten F", str(e))
+                            print(e)
                 except Exception as e:
-                    print("Ein Fehler ist aufgetreten G ", str(e))
+                    print(e)
             if len(span_1625) == 0:
                 span_1625.append(0)
             if len(span_1550) == 0:
@@ -103,12 +96,11 @@ try:
                 span_1310.append(0)
             OTDR_writer.writerow(
                 [
-                    #erste Zahl ändern, so dass nur Adresse in Adressspalte steht
                     address,
-                    rohr,
-                    round(mean(laenge), 3),
-                    round(max(laenge) - min(laenge), 2),
-                    laenge,
+                    pipe,
+                    round(mean(length), 3),
+                    round(max(length) - min(length), 2),
+                    length,
                     round(mean(span_1310), 3),
                     round(mean(span_1550), 3),
                     round(mean(span_1625), 3),
@@ -120,7 +112,7 @@ try:
         read_file.to_excel("OTDR_Excel.xlsx", index=None, header=True)
 
 except Exception as e:
-    print("Ein Fehler ist aufgetreten H ", str(e))
+    print(e)
 
 try:
     wb = load_workbook("OTDR_Excel.xlsx")
@@ -135,20 +127,20 @@ try:
     if argv["splices"]:
         if bool(re.compile(r"^[a-zA-Z][0-9]$").match(argv["splices"])):
             try:
-                return int(worksheet.cell(argv['splices']).value)
+                splices = int(ws.cell(argv['splices']).value)
             except Exception as e:
                 print(e)  
         else:
             try:
-                return int(argv["splices"])
+                splices = int(argv["splices"])
             except Exception as e:
                 print(e)
     for row in range(2, ws.max_row + 1):
-        laenge = ws.cell(row=row, column=3).value
+        length = ws.cell(row=row, column=3).value
         GW_splice = 0.2
-        GW_span_1310 = (0.36 * laenge + 0.45 + 0.7 + extra) + (splices * GW_splice)
-        GW_span_1550 = (0.21 * laenge + 0.45 + 0.7 + extra) + (splices * GW_splice)
-        GW_span_1625 = (0.25 * laenge + 0.45 + 0.7 + extra) + (splices * GW_splice)
+        GW_span_1310 = (0.36 * length + 0.45 + 0.7 + extra) + (splices * GW_splice)
+        GW_span_1550 = (0.21 * length + 0.45 + 0.7 + extra) + (splices * GW_splice)
+        GW_span_1625 = (0.25 * length + 0.45 + 0.7 + extra) + (splices * GW_splice)
         span_1310 = ws.cell(row=row, column=6).value
         span_1550 = ws.cell(row=row, column=7).value
         span_1625 = ws.cell(row=row, column=8).value
@@ -165,8 +157,8 @@ try:
                 start_color="00FF0000", fill_type="solid"
             )
 
-    wb.save("OTDR_Excel_geprueft"+ ".xlsx")
+    wb.save("OTDR_Excel_checked.xlsx")
 except Exception as e:
-    print("Ein Fehler ist aufgetreten K ",str(e))
+    print(e)
 
-print("Done ")
+print("Done")
